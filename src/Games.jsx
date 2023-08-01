d193945f88ffc31e54fdc1cc5d950bc369642053
Jakub Fałowski <kubafa99@gmail.com>
2023-08-01 22:45:09 +0200

    stylowanie

--- src/Games.jsx
+++ src/Games.jsx
@@ -1,3 +1,34 @@
 export function Games() {
-  return <div>games</div>;
+  return (
+    <div style={{ backgroundColor: "green", height: "100vh" }}>
+      <span
+        style={{
+          display: "inline-block",
+          backgroundColor: "brown",
+          border: "8px solid black",
+          width: " 350px",
+          height: "280px",
+          fontSize: "112px",
+          color: "white",
+          textTransform: "uppercase",
+        }}
+      >
+        ma
+      </span>
+      <span
+        style={{
+          display: "inline-block",
+          backgroundColor: "yellow",
+          border: "8px solid red-orange",
+          width: " 350px",
+          height: "280px",
+          fontSize: "112px",
+          textTransform: "uppercase",
+          textAlign: "right",
+        }}
+      >
+        flo
+      </span>
+    </div>
+  );
 }
